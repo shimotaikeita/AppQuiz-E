d3a2dce4b7707f16d177dc2d339e6f016f63066b
shimotai keita <shimotai.keita.sys@gmail.com>
2017-06-27 11:59:07 +0900

    fastcommit

---
+++ src/AppQuiz.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class AppQuiz {
+	static int n = 0;
+	static Scanner sc = new Scanner(System.in);
+	static int answer = 0;
+
+	//
+	//全合計点は5点 正解するたびにnで1点ずつカウント
+
+		public static void situmon1(){
+			//質問内容を表示
+			//ユーザーが答えを入力
+			//答えを表示
+			//カウントする
+			}
+		public static void situmon2(){
+			//質問内容を表示
+			//ユーザーが答えを入力
+			//カウントする
+		}
+		public static void situmon3(){
+			//質問内容を表示
+			//ユーザーが答えを入力
+			//答えを表示
+			//カウントする
+
+		}
+		public static void situmon4(){
+			//質問内容を表示
+			//ユーザーが答えを入力
+			//答えを表示
+			//カウントする
+
+		}
+		public static void situmon5(){
+			//質問内容を表示
+			//ユーザーが答えを入力
+			//答えを表示
+			//カウントする
+
+		}
+
+		public static void main(String[] args) {
+
+			situmon1();
+			situmon2();
+			situmon3();
+			situmon4();
+			situmon5();
+
+			System.out.println("合計点は" + n + "です");
+		}
+}
